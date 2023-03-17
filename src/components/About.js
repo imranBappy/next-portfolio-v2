906aac1e811ceb8ac765cc63050827b2e79b6c1c
imranBappy <imranbappy.official@gmail.com>
2023-03-18 00:43:29 +0600

    desing error fix

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <section id="about" className='w-full md:h-screen p-2 flex items-center py-16'>
             <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
 
-                <div className='col-span-2'>
+                <div className='col-span-2 px-2'>
                     <p className='uppercase text-xl tracking-wide text-primary'>About</p>
                     <h2 className='py-4'>Who I Am </h2>
                     <p className='py-2 text-gray-600'>&#47;&#47;  I am not your normal developer</p>
