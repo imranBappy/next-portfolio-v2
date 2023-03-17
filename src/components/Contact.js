99e63f770dd29ae4a5df54e35ef04fbc99a5f825
imranBappy <imranbappy.official@gmail.com>
2023-03-17 21:46:25 +0600

    added new pacekage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
