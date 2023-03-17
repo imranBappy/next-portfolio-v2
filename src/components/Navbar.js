564639de50196559f07710f308c2434c0b983a55
imranBappy <imranbappy.official@gmail.com>
2023-03-17 22:04:18 +0600

    import nav logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,7 +71,7 @@ const Navbar = () => {
                     : 'fixed left-[-100%] top-0  p-10 ease-in duration-500'}>
                     <div >
                         <div className='flex w-fill items-center justify-between'>
-                            <Image src='/../public/assets/navLogo.png' width={87} height={35} alt='imran' />
+                            <Image src={logo} width={87} height={35} alt='imran' />
                             <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                                 <AiOutlineClose />
                             </div>
